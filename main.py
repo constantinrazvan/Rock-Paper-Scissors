d83aca898c228d5430fb0cc6495edc88bf17d3c9
Razvan <76180811+constantinrazvan@users.noreply.github.com>
2022-11-14 00:50:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
 import time
 import os
+from userOneClass import playerOne
+from userTwoClass import playerTwo
 
 print("Rock, Papper, Scissors Game")
 time.sleep(3)
 os.system("clear")
-
-#Player one
-class playerOne:
-    def __init__(self):
-        self.username_playerOne = input('What is your name player one? \n')
-        self.player_one_score = 0
-
-      
-#Player two
-class playerTwo:
-    def __init__(self):
-        self.username_playerTwo = input('What is your name player two? \n')
-        self.player_two_score = 0
-
 
 #Init game
 def start_game():
